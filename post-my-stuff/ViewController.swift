89956bb851cb6261a8180a011eea15e9873d99a6
avot ron <ronavot@h-mac-mini-sl-ron.local>
2016-02-25 09:14:57 +0200

    Initial Commit

---
+++ post-my-stuff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  post-my-stuff
+//
+//  Created by ron avot on 25.2.2016.
+//  Copyright © 2016 ron avot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
